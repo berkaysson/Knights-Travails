ab0b6bbc5f9f06f0fa6d94dda477966e6f6dba01
berkaysson <berkaysonel85@gmail.com>
2023-06-23 21:46:59 +0300

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import ChessBoard from "./Chess/ChessBoard";
-import knightsMoves from "./knights";
 
 function App() {
-  console.log(knightsMoves([2,3], [6,7]).split(" -> "));
   return (
     <div className="App">
-      <ChessBoard />
+      <ChessBoard size={8} />
     </div>
   );
 }
